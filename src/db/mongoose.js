2ca297c03b728c5cd452a3ee60368be6f968f156
amitBalki <amitbalki16@gmail.com>
2020-05-11 13:30:59 +0000

    Elgibility in add-job fixed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
 
 //create connection to jobsDB
-mongoose.connect('mongodb://127.0.0.1:27018/tnp-web-portal-passport',{//"@cluster0-r6ge3.mongodb.net/jobsDB", {
+mongoose.connect(`mongodb+srv://${process.env.ATLASUSER}:${process.env.ATLASPASSWORD}@merncluster-mmtj5.mongodb.net/test?retryWrites=true&w=majority`,{//"@cluster0-r6ge3.mongodb.net/jobsDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
