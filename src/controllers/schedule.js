91765af012c03dc0c8cdecd07e5e9877652b1823
Amit Balki <amitbalki16@gmail.com>
2020-07-27 19:20:04 +0530

    fixed view-my-application frontend

--- src/controllers/schedule.js
+++ src/controllers/schedule.js
@@ -1,8 +1,6 @@
 const Schedule = require("../models/schedule")
-const Jobpost = require('../models/job')
 const Record = require('../models/record')
 const express = require("express")
-const router = express.Router();
 
 
 exports.createSchedule = (req, res) => {
@@ -20,11 +18,6 @@ exports.createSchedule = (req, res) => {
 
 exports.getAllSchedules = (req, res) => {
   Record.find({ student: req.user._id }).exec(async function findAndGet(err, records) {
-    // if (err) {
-    //   return res.status(400).json({
-    //     error: "Schedules not found"
-    //   });
-    // } else {
     try {
 
       const schedules = await Record.find({ student: req.user._id })
@@ -37,62 +30,14 @@ exports.getAllSchedules = (req, res) => {
 
 
       console.log(schedules);
-      //res.status(200).json(schedules);
-
-      // var jobposts = await getJobs(schedules);
-      // //console.log("First try: "+jobposts.length);
-      // var today = schedules.filter((a) => {
-      //   return a.date.getDate() == new Date().getDate() && a.date.getMonth() == new Date().getMonth() && a.date.getFullYear() == new Date().getFullYear();
-      // });
-
-      // var yesterday = schedules.filter((a) => {
-      //   return a.date.getDate() == new Date().getDate() - 1 && a.date.getMonth() == new Date().getMonth() && a.date.getFullYear() == new Date().getFullYear();
-      // });
-
-      // var upcoming = schedules.filter((a) => {
-      //   return a.date > new Date();
-      // });
       res.render('view-applications', {
-        // today: today,
-        // yesterday: yesterday,
-        // upcoming: upcoming,
-        // jobs: jobposts
         schedules: schedules
       });
 
     } catch (e) {
-      console.log(e);
-    }
-
-  });
-  //res.render();
-}
-
-// exports.getScheduleByJobId = (req, res) => {
-//   Schedule.find({ jobId: req.params.jobId }).exec((err, schedule) => {
-//     if (err) {
-//       return res.status(400).json({
-//         error: "Category not found in DB"
-//       });
-//     }
-//     res.status(200).json(schedule);
-
-//   })
-// };
-
-exports.getSchedules = (req, res) => {
-  Schedule.find().exec((err, schedule) => {
-    if (err) {
       return res.status(400).json({
-        error: "Category not found in DB"
-      });
+            error: "Schedules not found"
+          });
     }
-    schedule_ids = [];
-    schedule.forEach(element => {
-      schedule_ids.push(element._id);
-    });
-    console.log(schedule_ids)
-    res.status(200).json(schedule);
-
-  })
-};
+  });
+}
